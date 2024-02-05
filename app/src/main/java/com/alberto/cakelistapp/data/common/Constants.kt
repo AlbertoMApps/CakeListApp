b941da0a671a624649c1ab04c61ce9cef3e98484
Alberto Medrano <albersna@hotmail.com>
2024-02-05 13:06:54 +0000

    TEST:API

--- app/src/main/java/com/alberto/cakelistapp/data/common/Constants.kt
+++ app/src/main/java/com/alberto/cakelistapp/data/common/Constants.kt
@@ -1,4 +1,6 @@
 package com.alberto.cakelistapp.data.common
 
 const val CAKE_LIST_API_BASE_URL =
-    "https://gist.githubusercontent.com/t-reed/739df99e9d96700f17604a3971e701fa/raw/1d4dd9c5a0ec758ff5ae92b7b13fe4d57d34e1dc/waracle_cake-android-client/"
+    "https://gist.githubusercontent.com"
+const val CAKE_LIST_API_CLIENT =
+    "/t-reed/739df99e9d96700f17604a3971e701fa/raw/1d4dd9c5a0ec758ff5ae92b7b13fe4d57d34e1dc/waracle_cake-android-client"
--- app/src/main/java/com/alberto/cakelistapp/data/remote/api/CakeListApi.kt
+++ app/src/main/java/com/alberto/cakelistapp/data/remote/api/CakeListApi.kt
@@ -1,10 +1,11 @@
 package com.alberto.cakelistapp.data.remote.api
 
+import com.alberto.cakelistapp.data.common.CAKE_LIST_API_CLIENT
 import com.alberto.cakelistapp.data.remote.model.Cake
 import retrofit2.http.GET
 
 interface CakeListApi {
-    @GET()
+    @GET(CAKE_LIST_API_CLIENT)
     suspend fun getCakeList(): List<Cake>
 
 }
