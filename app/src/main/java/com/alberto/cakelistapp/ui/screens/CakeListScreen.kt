2029536d311c4c93fcf3eea347202734d8327f0d
Alberto Medrano <albersna@hotmail.com>
2024-02-05 12:41:27 +0000

    Implemented: Swip to refresh

--- app/src/main/java/com/alberto/cakelistapp/ui/screens/CakeListScreen.kt
+++ app/src/main/java/com/alberto/cakelistapp/ui/screens/CakeListScreen.kt
@@ -8,15 +8,35 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import com.alberto.cakelistapp.presentation.CakeListState
 import com.alberto.cakelistapp.presentation.CakeListViewModel
 import com.alberto.cakelistapp.ui.theme.CakeListAppTheme
+import com.google.accompanist.swiperefresh.SwipeRefresh
+import com.google.accompanist.swiperefresh.rememberSwipeRefreshState
 
 @Composable
 fun CakeListScreen(
     viewModel: CakeListViewModel = hiltViewModel()
 ) {
-    Text(
-        text = viewModel.cakeListState.collectAsState(initial = CakeListState()).value.data[0].title
-            ?: ""
-    )
+
+    val cakeListState = viewModel.cakeListState.collectAsState(initial = CakeListState()).value
+    val swipeRefreshState = rememberSwipeRefreshState(isRefreshing = cakeListState.isLoading)
+
+    when {
+        cakeListState.data.isNotEmpty() -> {
+            SwipeRefresh(
+                state = swipeRefreshState,
+                onRefresh = { viewModel.getCakeList() }
+            ) {
+                Text(
+                    text = cakeListState.data[0].title ?: ""
+                )
+            }
+        }
+
+        cakeListState.errorMessage.isNotEmpty() -> {
+            //TODO Error handling here...
+        }
+
+    }
+
 }
 
 @Preview(showBackground = true)
